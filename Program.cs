ba6fe7e481f3ee2029549fe67e1baa41c86b8dd9
Imaksymov <90256463+Imaksymov@users.noreply.github.com>
2021-10-18 18:19:14 +0300

    f

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static byte[] func(int length = 10)
+        {
+            var a = new RNGCryptoServiceProvider();
+            var d = new byte[length];
+            a.GetBytes(d);
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.WriteLine("--------------------------------------------");
+                Random d = new Random(245);
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(d.Next(0, 100));
+                }
+
+            }
+            Console.WriteLine("---------------------------------------------------");
+            for (int i = 0; i < 4; i++)
+            {
+                string text = Convert.ToBase64String(func());
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
