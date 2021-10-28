b585f6c02e8cd6f8004a44d0ac24d2ea324996bd
Imaksymov <90256463+Imaksymov@users.noreply.github.com>
2021-10-28 18:02:57 +0300

    Add files via upload

---
+++ lab3.3/lab3.3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab4_3
+{
+    class Program
+    {
+        public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key)
+        {
+            using (var hmac = new HMACSHA1(key))
+            {
+                return hmac.ComputeHash(toBeHashed);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const string message = "Hello!";
+            const string password = "DV0702";
+            const string message1 = "Hello!";
+            const string message2 = "hello";
+
+            var heshkode = ComputeHmacsha1(Encoding.Unicode.GetBytes(message), Encoding.Unicode.GetBytes(password));
+
+            Console.WriteLine($"Hash HMAC: {Convert.ToBase64String(heshkode)}");
+
+
+            var pHesh = heshkode;
+
+            var heshkode1 = ComputeHmacsha1(Encoding.Unicode.GetBytes(message1), Encoding.Unicode.GetBytes(password));
+
+            if (Convert.ToBase64String(pHesh) == Convert.ToBase64String(heshkode1))
+            {
+                Console.WriteLine("Message 1 is not changed");
+            }
+            else
+            {
+                Console.WriteLine("Message 1 is changed");
+            }
+
+            var heshkode2 = ComputeHmacsha1(Encoding.Unicode.GetBytes(message2), Encoding.Unicode.GetBytes(password));
+
+            if (Convert.ToBase64String(heshkode2) == Convert.ToBase64String(pHesh))
+            {
+                Console.WriteLine("Message 2 is not changed");
+            }
+            else
+            {
+                Console.WriteLine("Message 2 is changed");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab3.4/lab3.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab4_4
+{
+    class Program
+    {
+        static byte[] ComputeHashSHA256(byte[] dataForHash)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(dataForHash);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your login: ");
+            string log = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your password: ");
+            string pass = Convert.ToBase64String(ComputeHashSHA256(Encoding.Unicode.GetBytes(Convert.ToString(Console.ReadLine()))));
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Enter your login: ");
+            string log1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your password: ");
+            string pass1 = Convert.ToBase64String(ComputeHashSHA256(Encoding.Unicode.GetBytes(Convert.ToString(Console.ReadLine()))));
+
+            if (log != log1)
+            {
+                Console.WriteLine("Wrong login");
+            }
+            else if (pass != pass1)
+            {
+                Console.WriteLine("Wrong password");
+            }
+            else
+            {
+                Console.WriteLine("Login and password are correct");
+            }
+            Console.ReadKey();
+        }
+    }
+}
