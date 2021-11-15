9ffe480499618090ef5f0384d4ab5761f256400b
Imaksymov <90256463+Imaksymov@users.noreply.github.com>
2021-11-15 18:06:44 +0200

    Add files via upload

---
+++ lab5/lab5.4/lab5.4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5._4
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "something";
+            HashPassword(password, 150000);
+            HashPassword(password, 210000);
+            HashPassword(password, 260000);
+            HashPassword(password, 310000);
+            HashPassword(password, 360000);
+            HashPassword(password, 410000);
+            HashPassword(password, 460000);
+            HashPassword(password, 510000);
+            HashPassword(password, 560000);
+            HashPassword(password, 610000);
+            Console.ReadLine();
+        }
+        private static void HashPassword(string password, int numberOfRounds)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var hashedPassword = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(password), PBKDF2.GenerateSalt(), numberOfRounds, HashAlgorithmName.SHA512);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Password : " + password);
+            Console.WriteLine("Hashed Password : " + Convert.ToBase64String(hashedPassword));
+            Console.WriteLine("Iterations <" + numberOfRounds + "> Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+        }
+    }
+}
---
+++ lab5/lab5.5/lab5.5/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace _4part
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm, Int32 NumberOfBytes)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
+            {
+                return rfc2898.GetBytes(NumberOfBytes);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> logins = new List<string>();
+            List<string> passwords = new List<string>();
+            List<string> salts = new List<string>();
+            while (true)
+            {
+
+                Console.Write("Write 'l' to login or 'r' to register: ");
+                var temp = Console.ReadLine();
+                if (temp == "l")
+                {
+                    Console.Write("Enter your login: ");
+                    var login = Console.ReadLine();
+                    if (logins.Contains(login))
+                    {
+                        var ind = logins.IndexOf(login);
+                        Console.Write("Enter your password: ");
+                        var pass = Console.ReadLine();
+                        var salt = Convert.FromBase64String(salts[ind]);
+                        var sha256pass = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(pass), salt, 150000, HashAlgorithmName.SHA256, 32));
+                        if (passwords[ind] == sha256pass)
+                        {
+                            Console.WriteLine("Loggined");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong password");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong login");
+                    }
+                }
+                else if (temp == "r")
+                {
+                    Console.Write("Enter your login: ");
+                    var login = Console.ReadLine();
+                    if (!logins.Contains(login))
+                    {
+                        logins.Add(login);
+                        Console.Write("Enter your password: ");
+                        var pass = Console.ReadLine();
+                        var salt = PBKDF2.GenerateSalt();
+                        var sha256pass = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(pass), salt, 150000, HashAlgorithmName.SHA256, 32));
+                        passwords.Add(sha256pass);
+                        salts.Add(Convert.ToBase64String(salt));
+                        Console.WriteLine("Registered");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login already in base");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Write correct please");
+                }
+            }
+        }
+    }
+}
+
